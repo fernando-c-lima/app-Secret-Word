0b0cfbdf664d01d1b83948b2fe52473ed3c06fb5
fernando-c-lima <fernandox.caetano@gmail.com>
2024-07-11 23:31:13 -0300

    adicionando elemento inicias e estilizando botão

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import StartScreen from "./components/StartScreen";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Secret Word</h1>
+      <StartScreen />
     </div>
   );
 }
